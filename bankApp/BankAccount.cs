99aa07d1a9452f7cac19920606515f4cf9d41d4e
henninghk <64491564+henninghk@users.noreply.github.com>
2021-04-22 14:29:12 +0200

    Initial commit

---
+++ bankApp/BankAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace bankApp
+{
+    class BankAccount
+    {
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+        }
+    }
+}
